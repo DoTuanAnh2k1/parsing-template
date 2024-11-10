879daa6fb5544246839ae22c84b2634a4dc9d11c
chonlele <anh.dt2605@gmail.com>
2024-11-10 21:15:42 +0700

    [Update]

--- parsing/check_option.go
+++ parsing/check_option.go
@@ -1,12 +1,16 @@
 package parsing
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 func checkOption(template string) string {
 	re := regexp.MustCompile(`\[(.*?)\]`)
 
 	template = re.ReplaceAllStringFunc(template, func(s string) string {
-		if s == "[]" {
+		parts := strings.Split(s, ":")
+		if len(parts) < 2 || s == "[]" {
 			return ""
 		}
 		return s
